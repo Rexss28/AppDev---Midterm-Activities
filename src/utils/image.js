66377f0e89d9c646acc01fd584f35abaf578c2d1
Rexss28 <rexjohncab@gmail.com>
2026-04-22 11:43:07 +0800

    feat(components & utils): add CustomTextInput and CustomButton components

---
+++ src/utils/image.js
@@ -0,0 +1,6 @@
+export default {
+  LOGO: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg',
+  AUTH_BG: {
+    uri: 'https://scontent.fdgt1-1.fna.fbcdn.net/v/t39.30808-6/471261531_122180436302164656_4952909335634224378_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=b895b5&_nc_eui2=AeENCPlbRdrp2NlGq78-KSPdWC0WNo-pQB9YLRY2j6lAH6FVhq_5gIRQn5ByYJl9VIboRblhZFW9PLy-oa3BdzUa&_nc_ohc=ki041roLNywQ7kNvwFlzCbN&_nc_oc=AdmLoSh_pPuXNJHxjK-SppQUxZAEWfUzU0WIS7Vw8-aU4l2yUFwfbiqlAp9K58uptc&_nc_zt=23&_nc_ht=scontent.fdgt1-1.fna&_nc_gid=Ray117SqPmUnhoWtDFxECg&_nc_ss=8&oh=00_Afzyo8zJApO4RC3_DRU3tQpFwVQeUl9xkkjJaWZEDeVdGA&oe=69B82AD9',
+  },
+};
---
+++ src/utils/routes.js
@@ -0,0 +1,14 @@
+export default {
+  // Auth
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+
+  // Main
+  STORE: 'Store',
+  BROWSE: 'Browse',
+  CART: 'Cart',
+  LIBRARY: 'Library',
+
+  // Details
+  GAME_DETAILS: 'GameDetails',
+};
